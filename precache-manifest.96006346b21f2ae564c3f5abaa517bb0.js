7ad0c64f27cfe6df17e728cd94d61bbdf64d7fcf
unknown <seunyayu@gmail.com>
2023-10-19 12:47:17 +0100

    Updates

---
+++ precache-manifest.96006346b21f2ae564c3f5abaa517bb0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0e4d2fe633802471d0a98acc56a9cc",
+    "url": "/afrima_temps/index.html"
+  },
+  {
+    "revision": "69fa39096c4113aadd92",
+    "url": "/afrima_temps/static/css/2.15d8162b.chunk.css"
+  },
+  {
+    "revision": "12a176ce6bc4105ff93a",
+    "url": "/afrima_temps/static/css/main.7aed2a7f.chunk.css"
+  },
+  {
+    "revision": "69fa39096c4113aadd92",
+    "url": "/afrima_temps/static/js/2.6eb4eb9c.chunk.js"
+  },
+  {
+    "revision": "c702756c9cb0ce7eb52f",
+    "url": "/afrima_temps/static/js/3.d8ac33f5.chunk.js"
+  },
+  {
+    "revision": "12a176ce6bc4105ff93a",
+    "url": "/afrima_temps/static/js/main.bb4430b4.chunk.js"
+  },
+  {
+    "revision": "de198b03d36d3e3e89f0",
+    "url": "/afrima_temps/static/js/runtime~main.7e5df1d2.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/afrima_temps/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/afrima_temps/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/afrima_temps/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/afrima_temps/static/media/slick.f97e3bbf.svg"
+  }
+]);
